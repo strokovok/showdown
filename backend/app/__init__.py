de3a7e6d4164aef27e493ad9887c782bacd47057
DimasGhost <strokovghost@gmail.com>
2020-04-16 09:03:14 +0700

    Init backend

---
+++ backend/app/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+from flask import Flask
+import click
+from flask.cli import with_appcontext
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(app.instance_path, "showdown.sqlite"),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+    os.makedirs(app.instance_path, exist_ok=True)
+    app.config.from_pyfile("config.py", silent=True)
+
+    from . import database
+    database.init_app(app)
+
+    return app
---
+++ backend/app/database.py
@@ -0,0 +1,24 @@
+import click
+
+from flask.cli import with_appcontext
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+def init_app(app):
+    db.init_app(app)
+    app.cli.add_command(init_db_command)
+
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo("Initialized the database.")
