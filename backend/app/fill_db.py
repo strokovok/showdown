3db7d58ac34daf4b86f066da841a139851180615
DimasGhost <strokovghost@gmail.com>
2020-04-29 04:40:56 +0700

    Add demo users

--- backend/app/fill_db.py
+++ backend/app/fill_db.py
@@ -12,7 +12,7 @@ def init_app(app):
     app.cli.add_command(fill_db_command)
 
 
-def fill_db():
+def fill_db_old():
     games = []
     for i in range(3):
         game = Game(name=f'Game_{i}', manager_token=f'manager_token_{i}')
@@ -35,6 +35,28 @@ def fill_db():
             participants=random.sample(game.bots, k=2)
         )
         db.session.add(match)
+    db.session.commit()
+
+
+def fill_db():
+    tmp = {
+        "Алиса": ["Терминатор", "Мегатрон"],
+        "Боб": ["Пылесос", "Чайник"],
+        "Дмитрий": ["T-2000", "R2-D2"],
+        "Петр": ["С-3PO", "Эш"],
+        "Данил": ["Рой Бати", "Вертер"],
+        "Вадим": ["Бишоп", "Робокоп"],
+        "Владимир": ["Эндрю", "Бендер"],
+        "Сергей": ["Sonny NS5", "Оптимус"],
+    }
+    game = Game(name='Крестики нолики', manager_token='password')
+    db.session.add(game)
+    for login, bot_names in tmp.items():
+        user = User(login=login, password='password')
+        db.session.add(user)
+        for bot_name in bot_names:
+            bot = Bot(name=bot_name, rank=0, owner=user, game=game, access_token='password')
+            db.session.add(bot)
     db.session.commit()
 
 
