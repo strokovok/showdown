49e21509aa92b1c8f4d8b5473c0db1d1f38721d7
DimasGhost <strokovghost@gmail.com>
2020-05-29 21:23:55 +0700

    Update home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
     <div class="content-panel">
-        Добро пожаловать!
+        <div class="welcome">Добро пожаловать!</div>
+        <img class="showdown-img" src="/showdown.png">
+        Showdown.AI - это платформа для проведения соревнований по программированию ИИ.<br>
+        Здесь участники разрабатывают игровой ИИ и подключают его к системе соревнований, после чего происходит игра. <br>
+        Присоединяйтесь к соревнованиям или наблюдайте за играми!
+<!--        Наблюдайте за прошедшими соревнованиями и рейтингом игровых ботов.<br>-->
+<!--        Или присоединяйтесь к соревнованиям.<br>-->
     </div>
 </template>
 
 <style lang="scss" scoped>
     .content-panel {
-        width: 100%;
-        height: 500px;
-        background: $col-panel;
+        align-items: center;
+        text-align: center;
+        line-height: 1.5em;
+    }
+
+    .welcome {
+        font-size: 1.1em;
+    }
+
+    .showdown-img {
+        margin-top: 20px;
+        width: 40%;
     }
 </style>
 
 <script>
+    import VueMarkdown from 'vue-markdown';
+
     export default {
+        components: {
+            VueMarkdown
+        }
     }
 </script>
