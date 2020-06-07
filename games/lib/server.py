b658c8dd2d24dfd76cf355483a8696f74ef4fc36
DimasGhost <strokovghost@gmail.com>
2020-06-08 05:13:38 +0700

    Fix WS server host

--- games/lib/server.py
+++ games/lib/server.py
@@ -47,6 +47,6 @@ class WebSocketServer:
         client.on_close()
 
     def serve(self, port):
-        start_server = websockets.serve(self._serve_client, "localhost", port)
+        start_server = websockets.serve(self._serve_client, "*", port)
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
